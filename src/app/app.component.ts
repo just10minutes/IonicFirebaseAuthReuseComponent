8799c0d8d24d907e966a15267929d25bec957c87
Rajesh <just10minutes@gmail.com>
2017-09-15 22:45:09 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,36 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import firebase from 'firebase';
+
 
 import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    firebase.initializeApp({
+      apiKey: "AIzaSyCZ33opSUQie8zwvivIC7z8SLpYX2y2DBw",
+      authDomain: "myneweventmanagement.firebaseapp.com",
+      databaseURL: "https://myneweventmanagement.firebaseio.com",
+      storageBucket: "",
+      messagingSenderId: "43403241510"
+    });
+
+    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (!user) {
+        this.rootPage = 'login';
+        unsubscribe();
+      } else { 
+        this.rootPage = HomePage;
+        unsubscribe();
+      }
+    });
+
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
